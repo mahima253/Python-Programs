b8a2e6bb6da054edd4668ba1c5607f097609a99f
mahima253 <67818730+mahima253@users.noreply.github.com>
2020-07-15 11:42:31 +0530

    Project

---
+++ Positive numbers.py
@@ -0,0 +1,20 @@
+list1=[12,-7,5,64,14]
+list2=[12,14,-95,3]
+print("List 1:")
+for i in list1:
+    if i>0:
+        print(i)
+    else:
+        continue
+    
+
+
+
+print("List 2:")    
+for j in list2:
+    if j>0:
+        print(j)
+    else:
+        continue
+    
+    
